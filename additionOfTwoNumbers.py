885d6bf9539f63053b4039a513d8de89eed366e4
Johan20226 <88082303+Johan20226@users.noreply.github.com>
2021-10-24 11:15:32 +0200

    Delete additionOfTwoNumbers.py

--- additionOfTwoNumbers.py
+++
@@ -1,4 +0,0 @@
-numOne = input("Please enter your first number: ") # first number variable as a string
-numTwo = input("Please enter your second number: ") # second number variable as a string
-total = float(numOne) + float(numTwo) # variable conversion to float numbers
-print("Your first inputted number: {0} added with your second inputted number: {1} = {2}".format(numOne, numTwo, total)) # outputting the result
